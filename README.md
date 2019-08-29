b02b63d62401deb61644c1860353cb8f9ab8ca04
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-08-28 20:20:31 -0700

    everything working but swagger and tests

--- README.md
+++ README.md
@@ -7,23 +7,24 @@
  ### Links and Resources
 * [submission PR](https://github.com/evan-401-advanced-javascript/lab-13/pull/1)
 * [travis](https://www.travis-ci.com/evan-401-advanced-javascript/lab-13/builds/125003436)
-* [front-end](https://lab11-auth-server.herokuapp.com/) 
+* [deployment](https://lab-13-tokens.herokuapp.com/) 
 * [docs](http://localhost:3000/docs/)
 
 
  ##### Exported Values and Methods
 
  ###### 
-`get(books/:id) -> {object} 200 -  book title`
-`post(books) -> {object} 200 -  count and book results
-
 `get(signup) -> {object} 200 - request token`
 `get(signin) -> {object} 200 - request token`
 
 
-g### Setup
+### Setup
 #### `.env` requirements
-* `PORT` - 3003
+* `MONGODB_URI` - mongodb://localhost:27017/lab-13
+* `PORT` - 3000
+* `SECRET` - secret
+* `SINGLEUSE` - true
+* `EXPIRATION` - true
 
 
  #### UML
--- src/auth/router.js
+++ src/auth/router.js
@@ -7,6 +7,14 @@ const User = require('./users-model.js');
 const auth = require('./middleware.js');
 const oauth = require('./oauth/google.js');
 
+/**
+ * This is a route to sign up a user
+ * @route POST /signup
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ * @returns {object} returns token
+ */
 authRouter.post('/signup', (req, res, next) => {
   let user = new User(req.body);
   user.save()
@@ -19,11 +27,26 @@ authRouter.post('/signup', (req, res, next) => {
     }).catch(next);
 });
 
+/**
+ * This is a route to sign in
+ * @route POST /signin
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ * @returns {object} 200 -  returns token
+ */
 authRouter.post('/signin', auth, (req, res, next) => {
   res.cookie('auth', req.token);
   res.send(req.token);
 });
 
+/**
+ * This is a route to use Oauth
+ * @route POST /oath
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ */
 authRouter.get('/oauth', (req,res,next) => {
   oauth.authorize(req)
     .then( token => {
